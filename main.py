08c83db40ccf9b399541063fd30ffa70b8c62cd0
Wiktoria1626 <74345324+Wiktoria1626@users.noreply.github.com>
2020-11-24 18:14:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 # This is a sample Python script.
-
+#############################################################
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-#Piszę coś na próbe ....... 
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
