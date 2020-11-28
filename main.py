0cc1258c8976a3ac72696b3ae3966858791d76c4
Wiktoria1626 <74345324+Wiktoria1626@users.noreply.github.com>
2020-11-28 11:13:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-# Wiktoria Czechonska
-# Kinga Witschenbach
-print('hi')
+print(“Symulacja ruchu ciał niebieskich z wykorzystaniem Praw Keplera”)     
+print(“Kinga Witschenbach”)
+print(“Wiktoria Czechońska”)
+
+# w wstępie 
+#ewentualne ciekawostki co do ciał niebieskich i Keplera
+
+#napisany kod co do praw keplera ich treści z wyborem przez użytkownika
+#do wyboru 1 2 3 lub zakończ 
+
+#zaimportowany plik z danymi planet 
+#możliwość obliczeń ich w wzorach przez użytkownika 
+
+#przedstawienie ruchu planet wokół słońca
+#ewentualny wykres
+
+#przedstawienie symulacji ruchu ciał na wykresie 
+
+#jako dodatek do programu gra o temacie astronomii
+
+# Ewentualny dodatkowy program...
+
+#ciekawe zakończenie programu 
